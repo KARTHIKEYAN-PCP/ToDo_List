3c6622cff1bd5b1f76f1f8227614f9d8222b93a6
karthikeyan-pcp <karthi268004@gmail.com>
2022-01-16 17:42:25 +0530

    hii

--- base/views.py
+++ base/views.py
@@ -1,19 +1,21 @@
-from asyncio import Task
-from re import template
-from turtle import title
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Task
-from django.views.generic.edit import CreateView , UpdateView ,DeleteView ,FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-from django.contrib.auth.views import LoginView
+# Imports for Reordering Feature
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Task
+from .forms import PositionForm
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
