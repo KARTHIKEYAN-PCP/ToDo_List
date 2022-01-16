71a4396f617dc3be7bd590f85941d965a8a8cf95
karthikeyan-pcp <karthi268004@gmail.com>
2022-01-16 17:36:05 +0530

    hii

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT =os.path.join(BASE_DIR,'assets')
 
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
